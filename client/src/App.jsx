36f9e938917b160ba72795f7a2341a4e8055a512
Hardik Patil <hardikpatil894800@gmail.com>
2025-05-30 21:59:21 +0530

    create a react+vite app

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className='bg-blue-400'>Hey</h1>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/app/userSlice.js
@@ -0,0 +1,12 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+    Data:[]
+}
+
+export const userSlice = createSlice({
+     //data
+})
+
+
+export default userSlice.reducer
